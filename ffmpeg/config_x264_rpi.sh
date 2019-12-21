0187bf639033e4df234b83002040eb56f0afb758
rankun <rankun@bytedance.com>
2019-12-22 07:59:43 +0800

    feat: add x264 script

---
+++ ffmpeg/config_x264_rpi.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./configure --disable-shared --enable-static --enable-strip --disable-cli
---
+++ ffmpeg/install_x264.sh
@@ -0,0 +1,66 @@
+# 获取绝对路径，保证其他目录执行此脚本依然正确
+{
+cd $(dirname "$0")
+script_path=$(pwd)
+cd -
+} &> /dev/null # disable output
+
+# 设置当前目录，cd的目录影响接下来执行程序的工作目录
+old_cd=$(pwd)
+cd $(dirname "$0")
+
+echo
+echo ---------------------------------------------------------------
+echo clone x264
+echo ---------------------------------------------------------------
+
+x264_path=$script_path/x264
+if [ ! -d $x264_path ];then
+    git clone https://code.videolan.org/videolan/x264.git
+else
+    echo x264 exist
+fi
+
+if [ $? -ne 0 ]; then
+    echo clone x264 failed
+    exit 1
+fi
+
+echo
+echo ---------------------------------------------------------------
+echo config x264
+echo ---------------------------------------------------------------
+
+cd x264
+mv ../config_x264_rpi.sh ./
+chmod +x config_x264_rpi.sh
+./config_x264_rpi.sh
+
+echo
+echo ---------------------------------------------------------------
+echo make x264
+echo ---------------------------------------------------------------
+
+make -j4
+
+if [ $? -ne 0 ]; then
+    echo make x264 failed
+    exit 1
+fi
+
+echo
+echo ---------------------------------------------------------------
+echo install x264
+echo ---------------------------------------------------------------
+
+sudo make install
+
+if [ $? -ne 0 ]; then
+    echo install x264 failed
+    exit 1
+fi
+
+# 恢复工作目录
+cd $old_cd
+
+echo install x264 success
